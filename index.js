16f0e0b8cad2672329df9cc1201caa0a6a6317ff
YeFan <919024797@qq.com>
2021-04-25 15:03:59 +0800

    feat(跨域): 设置允许跨域

--- index.js
+++ index.js
@@ -17,8 +17,21 @@ app.use('/user', require('./router/user'))
 // 引入list.js
 app.use('/list', require('./router/list'))
 
+// 设置请求跨域
+// app.all('*', function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');  //设置允许跨域的域名，*代表允许任意域名跨域
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');//允许的header类型
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS'); //跨域允许的请求方式
+//     if (req.method === 'OPTIONS') {
+//         res.send(200);//让options尝试请求快速结束
+//     } else {
+//         next();
+//     }
+// })
+
 // 监听3000端口
 let server = app.listen(3000, () => {
+    console.log(server.address())
     let host = server.address().address // host域
     let port = server.address().port // 端口号
 
